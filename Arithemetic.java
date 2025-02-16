f6f1cc7cb703f65e1bea31565e63ca2cc2eee090
Saloni narkar <saloninarkar113@gmail.com>
2025-02-16 14:34:08 +0530

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,22 @@
+
+class Arithmetic
+{
+    public static void main(String[]args) 
+    {
+        int a=10;
+        int b=20;
+        float c=12.5f;
+        double d=10.4;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+        System.out.println("a+b="+(a+b));
+        System.out.println("b-a="+(b-a));
+        System.out.println("c/a="+(c/a));
+        System.out.println("b%a="+b%a);
+        System.out.println("c*d="+c*d);
+
+
+    }
+}
